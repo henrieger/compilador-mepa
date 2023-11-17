87eda83f8a53934ff1c6b48a69df127dd990816f
Henrique Rieger <henriquerieger2001@gmail.com>
2023-11-16 22:58:23 -0300

    [add] atribuição de funções

--- regras/regra19.c
+++ regras/regra19.c
@@ -7,33 +7,51 @@
 void atribuicao()
 {
   // Recupera símbolo a ser atribuído
-  char variavel[TAM_TOKEN];
-  pop(pilhaIdents, variavel, TAM_TOKEN);
-  attrsSimbolo_t *attr = buscaSimbolo(tabelaSimbolos, variavel);
+  char simbolo[TAM_TOKEN];
+  pop(pilhaIdents, simbolo, TAM_TOKEN);
+  attrsSimbolo_t *attr = buscaSimbolo(tabelaSimbolos, simbolo);
 
   // Verifica existência e tipo do símbolo
   if (!attr)
-    printErro("Variável %s não definida", variavel);
+    printErro("Símbolo %s não definido", simbolo);
 
-  if (attr->cat != VAR_SIMPLES && attr->cat != PARAM_FORMAL)
-    printErro("Símbolo %s não é variável nem parâmetro formal", variavel);
+  if (attr->cat != VAR_SIMPLES && attr->cat != PARAM_FORMAL && attr->cat != FUNCAO)
+    printErro("Símbolo %s não é atribuível", simbolo);
 
   // Coloca símbolo na pilha de atributos
   push(pilhaAttrs, (void *) attr, sizeof(attrsSimbolo_t));
 
   // Recupera e compara tipo da expressão
   tipoDado_t tipoExpressao = popTipo(pilhaTipos);
-  
+  tipoDado_t tipoSimbolo;
+  switch (attr->cat)
+  {
+    case VAR_SIMPLES:
+      tipoSimbolo = attr->vsAttr.tipo;
+      break;
+    case PARAM_FORMAL:
+      tipoSimbolo = attr->pfAttr.tipo;
+      break;
+    case FUNCAO:
+      tipoSimbolo = attr->funAttr.tipoRetorno;
+      break;
+    default:
+      break;
+  }
+
   # ifdef DEBUG
-  printf("Tipo da expressao: %d\nTipo da variavel: %d\n", tipoExpressao, attr->vsAttr.tipo);
+    printf("Tipo da expressao: %d\nTipo da simbolo: %d\n", tipoExpressao, tipoSimbolo);
   # endif
       
-  if (attr->vsAttr.tipo != tipoExpressao)
-    yyerror("Tipo da expressão incompatível com a variável");
+  
+  if (tipoSimbolo != tipoExpressao)
+    yyerror("Tipo da expressão incompatível com o símbolo");
 
   // Gera comando para armazenar valor da expressão
   if (attr->cat == VAR_SIMPLES)
     printComando(NULL, "ARMZ %d, %d", attr->nivel, attr->vsAttr.desloc);
+  else if (attr->cat == FUNCAO)
+    printComando(NULL, "ARMZ %d, %d", attr->nivel, attr->funAttr.desloc);
   else if (attr->cat == PARAM_FORMAL)
   {
     if (attr->pfAttr.porRef)
